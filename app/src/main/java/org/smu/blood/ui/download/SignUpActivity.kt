d6c1491959e6868378c4c8576a29f7ee0d80944c
min <dearkimm@daum.net>
2021-11-15 14:28:21 +0900

    signup 수정

--- app/src/main/java/org/smu/blood/ui/download/SignUpActivity.kt
+++ app/src/main/java/org/smu/blood/ui/download/SignUpActivity.kt
@@ -14,34 +14,21 @@ import com.google.android.gms.tasks.OnFailureListener
 
 import com.google.android.gms.tasks.OnSuccessListener
 
-<<<<<<< HEAD
 import User
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 
 
-class SignUpActivity : AppCompatActivity() {
-=======
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
-import org.smu.blood.database.User
-
 
 class SignUpActivity : AppCompatActivity() {
     lateinit var mDatabase: DatabaseReference
 
->>>>>>> master
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
 
-<<<<<<< HEAD
-        var mDatabase: DatabaseReference// ...
-=======
 
-
->>>>>>> master
         mDatabase = FirebaseDatabase.getInstance().getReference();
 
 
@@ -51,32 +38,16 @@ class SignUpActivity : AppCompatActivity() {
         //계정 생성 이후 로그인으로 돌아가기
         joinButton.setOnClickListener {
 
-<<<<<<< HEAD
-
-=======
-            //writeNewUser("test1234", "테스트", "test@aa.aa")
->>>>>>> master
+            writeNewUser("test1234", "테스트", "test@aa.aa")
 
             finish()
         }
 
-<<<<<<< HEAD
-        fun writeNewUser(userId: String, name: String, email: String) {
-            val user = User(name, email)
-            mDatabase.child("users").child(userId).setValue(user)
-                .addOnSuccessListener(OnSuccessListener<Void?> { // Write was successful!
-                    Toast.makeText(this@SignUpActivity, "저장을 완료했습니다.", Toast.LENGTH_SHORT).show()
-                })
-                .addOnFailureListener(OnFailureListener { // Write failed
-                    Toast.makeText(this@SignUpActivity, "저장을 실패했습니다.", Toast.LENGTH_SHORT).show()
-                })
-        }
     }
 
-=======
 
-    }
-    /*fun writeNewUser(userId: String, name: String, email: String) {
+
+    fun writeNewUser(userId: String, name: String, email: String) {
         val user = User(name, email)
         mDatabase.child("users").child(userId).setValue(user)
             .addOnSuccessListener(OnSuccessListener<Void?> { // Write was successful!
@@ -85,7 +56,6 @@ class SignUpActivity : AppCompatActivity() {
             .addOnFailureListener(OnFailureListener { // Write failed
                 Toast.makeText(this@SignUpActivity, "저장을 실패했습니다.", Toast.LENGTH_SHORT).show()
             })
-    }*/
->>>>>>> master
+    }
 
 }
