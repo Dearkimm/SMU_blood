0d38cb27e807a89bca27febc22bde8298dc44a18
Jiseondf <wpdldptm109@naver.com>
2021-11-23 14:14:13 +0900

    jiseon

--- app/src/main/java/org/smu/blood/ui/my/MyActivity.kt
+++ app/src/main/java/org/smu/blood/ui/my/MyActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import org.smu.blood.MyCardActivity
 import org.smu.blood.R
 
 class MyActivity : AppCompatActivity() {
@@ -16,6 +17,7 @@ class MyActivity : AppCompatActivity() {
         var modButton = findViewById<Button>(R.id.btn_mod1)
         var logout = findViewById<TextView>(R.id.tv_r)
         var withdraw = findViewById<TextView>(R.id.tv_withdraw)
+        var card = findViewById<TextView>(R.id.tv_q)
 
         //수정하러 가기
         modButton.setOnClickListener {
@@ -23,11 +25,16 @@ class MyActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-        //버튼
+        //카드 기록 보기
+        card.setOnClickListener {
+            val intent = Intent(this, MyCardActivity::class.java)
+            startActivity(intent)
+        }
+
 
         //로그아웃 팝업
         logout.setOnClickListener {
-            val dlg = MyLogoutDialog(applicationContext)
+            val dlg = MyLogoutDialog(this)
             dlg.callFunction()
             dlg.show()
 
@@ -38,7 +45,7 @@ class MyActivity : AppCompatActivity() {
 
         //회원탈퇴 팝업
         withdraw.setOnClickListener {
-            val dlg = MyWithdraw(applicationContext)
+            val dlg = MyWithdraw(this)
             dlg.callFunction()
             dlg.show()
 
