0f635d2688943b7d17234534914227c75b8d578a
jeongjaeyoon <jyjoung2000@naver.com>
2021-11-26 15:25:09 +0900

    리사이클러뷰 후기글 읽어오기

--- app/src/main/java/org/smu/blood/ui/board/BoardRegisterActivity.kt
+++ app/src/main/java/org/smu/blood/ui/board/BoardRegisterActivity.kt
@@ -6,6 +6,13 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import org.smu.blood.NavigationActivity
 import org.smu.blood.R
+import org.smu.blood.ui.board.BoardRegisterAlert
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.content.ContentProviderCompat.requireContext
+
 
 class BoardRegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,9 +24,16 @@ class BoardRegisterActivity : AppCompatActivity() {
         //글 작성 후 메인으로 이동
         registerButton.setOnClickListener {
             //글 작성 버튼 누르면 팝업 띄워주고
+            val dlg = BoardRegisterAlert(this)
+            dlg.callFunction()
+            dlg.show()
 
+            dlg.setOnDismissListener {
+
+            }
             //메인으로 이동하고
-            finish()
+            //finish()
+
         }
     }
 }
