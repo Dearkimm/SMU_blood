5d1200a13dc2b202cce594fb1ceae7daa0574ad5
Jiseondf <wpdldptm109@naver.com>
2021-11-30 11:43:53 +0900

    binding

--- app/src/main/java/org/smu/blood/ui/main/MainRequestFragment.kt
+++ app/src/main/java/org/smu/blood/ui/main/MainRequestFragment.kt
@@ -15,6 +15,11 @@ class MainRequestFragment : BaseFragment<FragmentMainRequestBinding>() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         configureRequestNavigation()
+
+        //뷰바인딩 쓸 수 있게 되어있어서 findViewById말로 binding 쓰시면 되고
+        //프래그먼트에서는 여기서 쓰시면 돼요
+        //얘처럼...binding.xml변수이름카멜식.메소드이름  이런 식으로
+       binding.checkbox1.setOnClickListener {  }
     }
 
     private fun configureRequestNavigation() {
