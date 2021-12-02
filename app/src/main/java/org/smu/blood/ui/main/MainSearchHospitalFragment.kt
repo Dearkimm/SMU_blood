c0df304a0cd158b19e3c2d00fabe85695876c132
min_aus <dearkimm@daum.net>
2021-12-02 19:12:28 +0900

    ??

--- app/src/main/java/org/smu/blood/ui/main/MainSearchHospitalFragment.kt
+++ app/src/main/java/org/smu/blood/ui/main/MainSearchHospitalFragment.kt
@@ -5,23 +5,27 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+
+
 import org.smu.blood.ui.NavigationActivity
 import org.smu.blood.R
 import org.smu.blood.databinding.FragmentMainSearchHospitalBinding
 import org.smu.blood.ui.base.BaseFragment
 
 class MainSearchHospitalFragment : BaseFragment<FragmentMainSearchHospitalBinding>() {
-
+    var mainRequestFragment: MainRequestFragment = MainRequestFragment()
     override fun initBinding(inflater: LayoutInflater, container: ViewGroup?) =
         FragmentMainSearchHospitalBinding.inflate(inflater, container, false)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-
+        var bundle : Bundle = Bundle()
+        var naviActivity = activity as NavigationActivity
 
         //ㄱ
         binding.hosRBt1.setOnClickListener {
             binding.hosRBt1.setBackgroundResource(R.drawable.bg_btn_red_5dp)
             binding.hosRBt1.setTextColor(Color.WHITE)
+            naviActivity.setDataAtFragment(mainRequestFragment, binding.hosRBt1.text.toString())
             (activity as NavigationActivity).popMainSearchHospital()
         }
         binding.hosRBt2.setOnClickListener {
@@ -341,4 +345,6 @@ class MainSearchHospitalFragment : BaseFragment<FragmentMainSearchHospitalBindin
 
 
     }
+
+
 }
