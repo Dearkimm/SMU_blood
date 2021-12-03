d67751af57bbdd07eeef026fa6e09fa34c2f4925
jeongjaeyoon <jyjoung2000@naver.com>
2021-12-03 12:46:23 +0900

    로그아웃,탈퇴 다이얼로그

--- app/src/main/java/org/smu/blood/ui/my/MyWithdraw.kt
+++ app/src/main/java/org/smu/blood/ui/my/MyWithdraw.kt
@@ -4,11 +4,13 @@ import android.app.Dialog
 import android.content.Context
 import android.view.WindowManager
 import android.widget.Button
+import android.widget.Toast
 import org.smu.blood.R
 
 class MyWithdraw(context: Context) :
     Dialog(context, android.R.style.Theme_Translucent_NoTitleBar){
     var dialog: MyWithdraw? = null
+    var withdrawState = false
 
     fun callFunction() {
         val lpWindow = WindowManager.LayoutParams()
@@ -18,11 +20,21 @@ class MyWithdraw(context: Context) :
         setContentView(R.layout.dialog_withdraw_alert)
         dialog = this
         var ok = findViewById<Button>(R.id.btn_not)
+        var withdraw = findViewById<Button>(R.id.btn_withdraw)
 
-        ok.setOnClickListener {
+        ok.setOnClickListener { //탈퇴하기 취소
+            withdrawState = false
+            returnState()
             dismiss()
         }
-
+        withdraw.setOnClickListener { //탈퇴하기
+            Toast.makeText(context,"탈퇴 되었습니다", Toast.LENGTH_SHORT).show()
+            withdrawState = true
+            returnState()
+            dismiss()
+        }
+    }fun returnState(): Boolean{
+        return withdrawState
     }
 
 }
