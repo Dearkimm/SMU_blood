78928714ca626009843429f99263fa2bb0ad2696
jeongjaeyoon <jyjoung2000@naver.com>
2021-12-05 14:10:35 +0900

    ,,

--- app/src/main/java/org/smu/blood/ui/map/MapApplicationActivity.kt
+++ app/src/main/java/org/smu/blood/ui/map/MapApplicationActivity.kt
@@ -53,12 +53,6 @@ class MapApplicationActivity : AppCompatActivity() {
             dlg.setOnDismissListener {
                 confirmState = dlg.returnState()
                 if(confirmState){ //건너뛰기
-<<<<<<< Updated upstream
-                    finish()
-                }
-                else{ //마이페이지에서 신청 확인하기
-                    finish()
-=======
                     val intent = Intent(this, NavigationActivity::class.java)
                     startActivity(intent)
                     this.finish()
@@ -67,7 +61,6 @@ class MapApplicationActivity : AppCompatActivity() {
                     val intent = Intent(this, MyRequestFragment::class.java)
                     startActivity(intent)
                     this.finish()
->>>>>>> Stashed changes
                 }
             }
         }
