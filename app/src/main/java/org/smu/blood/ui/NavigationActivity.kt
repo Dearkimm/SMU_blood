3d827c1863a6613ab25bc4706e005fcf2888668c
kmebin <blowhuiee@gmail.com>
2021-12-06 19:57:38 +0900

    [fix] 이미 추가된 fragment 오류 수정

--- app/src/main/java/org/smu/blood/ui/NavigationActivity.kt
+++ app/src/main/java/org/smu/blood/ui/NavigationActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.fragment.app.Fragment
+import org.smu.blood.R
 import org.smu.blood.databinding.ActivityNavigationBinding
 import org.smu.blood.ui.board.BoardFragment
 import org.smu.blood.ui.main.MainFragment
@@ -34,14 +36,18 @@ class NavigationActivity : AppCompatActivity() {
 
     private fun configureBottomNav() {
         binding.btnMain.setOnClickListener {
-            replaceFragment(binding.fragmentContainer, MainFragment::class.java, withAnim = false)
+            changeFragment(MainFragment())
         }
         binding.btnMap.setOnClickListener {
-            replaceFragment(binding.fragmentContainer, MapFragment::class.java, withAnim = false)
+            changeFragment(MapFragment())
         }
         binding.btnReview.setOnClickListener {
-            replaceFragment(binding.fragmentContainer, BoardFragment::class.java, withAnim = false)
+            changeFragment(BoardFragment())
         }
+    }
+
+    private fun changeFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(R.id.fragment_container, fragment).commit()
     }
 
     fun navigateMainToRequest(){
