707470acfa4685b22fe01ffea1a19bdcd6f79308
jeongjaeyoon <jyjoung2000@naver.com>
2022-02-02 23:30:27 +0900

    게시글 수정삭제 팝업메뉴

--- app/src/main/java/org/smu/blood/ui/board/CommentData.kt
+++ app/src/main/java/org/smu/blood/ui/board/CommentData.kt
@@ -3,6 +3,6 @@ package org.smu.blood.ui.board
 data class CommentData( //댓글 데이터
     val id : String,
     val nickname : String,
-    val time : String,
+    val time : String, //댓글 쓴 날짜
     val comment : String
 )
