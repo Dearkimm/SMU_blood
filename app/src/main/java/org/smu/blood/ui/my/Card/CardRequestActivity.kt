74f2a6e60d635ef897acc55dd30e39510b4c0077
jeongjaeyoon <jyjoung2000@naver.com>
2022-02-20 15:58:21 +0900

    ..

--- app/src/main/java/org/smu/blood/ui/my/Card/CardRequestActivity.kt
+++ app/src/main/java/org/smu/blood/ui/my/Card/CardRequestActivity.kt
@@ -1,7 +1,11 @@
 package org.smu.blood.ui.my.Card
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.annotation.Dimension
 import androidx.appcompat.app.AppCompatActivity
 import org.smu.blood.R
 
@@ -11,6 +15,23 @@ class CardRequestActivity : AppCompatActivity() {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_card_request)
+
+        val timelist = findViewById<LinearLayout>(R.id.card_request_list)
+        for(i: Int in 1..3){
+            val time = TextView(this@CardRequestActivity)
+            val layoutParams = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+            )
+            layoutParams.setMargins(0,0,0,5)
+            time.layoutParams = layoutParams
+            time.text = "$i 텍스트뷰" //요청 일시넣기
+            time.setTextSize(Dimension.SP,13.0f)
+            time.typeface = resources.getFont(R.font.notosans_light)
+            time.includeFontPadding = false
+            time.setTextColor(Color.BLACK)
+            timelist.addView(time)
+        }
 
     }
 }
