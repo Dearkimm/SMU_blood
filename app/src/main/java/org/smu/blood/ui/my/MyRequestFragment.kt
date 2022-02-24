b0841c98909f00fd77c7ce7822f9676bd6daf4f4
koharin <dudu@sookmyung.ac.kr>
2022-02-25 02:40:04 +0900

    Edit get request list of apply in card, add setting of fcm

--- app/src/main/java/org/smu/blood/ui/my/MyRequestFragment.kt
+++ app/src/main/java/org/smu/blood/ui/my/MyRequestFragment.kt
@@ -23,7 +23,7 @@ import org.smu.blood.ui.my.Card.CardRequestActivity
 class MyRequestFragment : BaseFragment<FragmentMyRequestBinding>() {
     //카드 기록 상태(0->요청, 1->등록)
     var cardState = 0
-    lateinit var intent : Intent
+
     //어댑터
     private val myCardAdapter = MainRequestAdapter()
 
@@ -99,38 +99,35 @@ class MyRequestFragment : BaseFragment<FragmentMyRequestBinding>() {
             binding.recRequest.setTextColor(Color.BLACK)
             cardState = 1
 
+            myCardAdapter.setItems(emptyList())
             // DB에서 내 신청 리스트 가져오기
             MainService(requireContext()).myApplyList { myApplyList ->
+                // 리스트 초기화
+                myCardAdapter.setItems(emptyList())
+
                 if(myApplyList!=null){
                     Log.d("[MY APPLY LIST]", "GET LIST")
                     // 내 신청 정보 리스트에 넣기
                     applyList = myApplyList
-                    val mainRequestList = mutableListOf<MainRequest>()
-                    val requestListForApply = mutableListOf<MainRequest>()
 
                     // 서버에서 내가 신청한 요청은 가져오는데 mainRequest 리스트에 안 들어가서 신청 기록 안 보여줌 (수정 필요)
                     for(apply in myApplyList){
                         // get request of my apply
                         MainService(requireContext()).requestOfApply(apply.requestId!!){ request ->
                             if(request!=null){
+                                // Request 객체를 requestList에 넣기
+                                requestList.add(request)
                                 Log.d("[REQUEST OF MY APPLY] GET REQUEST", "$request")
+
                                 // 내가 신청한 지정 헌혈 요청 정보 MainRequest 리스트에 넣기
                                 val mainRequest = MainRequest(request.hospitalId!!, request.requestId!!, request.rhType!!, request.bloodType!!, request.donationType!!, request.startDate!!, request.endDate!!, request.applicantNum!!, request.story!!, request.registerTime!!)
                                 Log.d("[MY REQUEST LIST]", mainRequest.toString())
-                                // 각 request 정보 MainRequest 리스트에 넣기
-                                mainRequestList.add(mainRequest)
 
-                                // Request 객체를 requestList에 넣기
-                                requestList.add(request)
+                                // 각 request 정보 MainRequest 리스트에 넣기 - setItems로 계속 안 돼서 addItems 사용
+                                myCardAdapter.addItems(mutableListOf(mainRequest))
                             }
                         }
                     }
-                    // 신청 기록에 보여줄 MainRequest 리스트 세팅
-                    mainRequestList.forEach {
-                        Log.d("[REQUEST OF MY APPLY] requestListForApply", it.toString())
-                    }
-                    myCardAdapter.setItems(mainRequestList)
-                    //myCardAdapter.setItems(requestListForApply)
                 }else{
                     Log.d("[REQUEST OF MY APPLY]", "GET APPLY LIST FAILURE")
                 }
@@ -152,6 +149,7 @@ class MyRequestFragment : BaseFragment<FragmentMyRequestBinding>() {
                 myRequest  = requestList.first { it.requestId ==  requestInfo.requestId }
 
                 //(activity as NavigationActivity).navigateMainToRead()
+                val intent: Intent
                 if(cardState == 0) intent = Intent(context, CardRequestActivity::class.java)
                 else intent = Intent(context, CardApplyActivity::class.java)
 
@@ -198,17 +196,5 @@ class MyRequestFragment : BaseFragment<FragmentMyRequestBinding>() {
         lateinit var applyList: List<Apply> // 신청 정보 리스트
         var requestList = mutableListOf<Request>() // 요청 기록 카드에 필요
         lateinit var myRequest: Request // 요청 기록 카드, 신청 기록 카드에 필요
-        /*
-        var hospitalId = -1
-        var rhType = false
-        var bloodType = -1
-        var donationType = ""
-        var startDate = ""
-        var endDate = ""
-        var count = -1
-        var content = ""
-        var updatedDate = ""
-
-         */
     }
 }
