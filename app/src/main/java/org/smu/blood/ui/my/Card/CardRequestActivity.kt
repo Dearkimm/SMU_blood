d7e5695b3814c217fab6b6990edd5f5476ef99ed
jeongjaeyoon <jyjoung2000@naver.com>
2022-02-27 17:25:46 +0900

    ..

--- app/src/main/java/org/smu/blood/ui/my/Card/CardRequestActivity.kt
+++ app/src/main/java/org/smu/blood/ui/my/Card/CardRequestActivity.kt
@@ -1,19 +1,28 @@
 package org.smu.blood.ui.my.Card
 
+import android.content.DialogInterface
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.view.WindowManager
+import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.annotation.Dimension
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import org.smu.blood.R
+import org.smu.blood.databinding.ActivityCardRequestBinding
+import org.smu.blood.util.shortToast
 
 class CardRequestActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCardRequestBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
+        binding = ActivityCardRequestBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_card_request)
 
         val timelist = findViewById<LinearLayout>(R.id.card_request_list)
@@ -32,6 +41,26 @@ class CardRequestActivity : AppCompatActivity() {
             time.setTextColor(Color.BLACK)
             timelist.addView(time)
         }
+        val btn = findViewById<Button>(R.id.request_delete)
+        btn.setOnClickListener{
+            Log.d("sd","ss")
+            shortToast("클릭")
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("헌혈을 받으셨나요?")
+            builder.setMessage("헌혈 요청 글이 삭제됩니다.")
+            var listener = DialogInterface.OnClickListener { dialog, p1 ->
+                when (p1) {
+                    DialogInterface.BUTTON_POSITIVE ->
+                        shortToast("요청을 삭제했습니다")
+                    DialogInterface.BUTTON_NEGATIVE ->
+                        shortToast("취소")
+                }
+            }
+            builder.setPositiveButton("확인",listener)
+            builder.setNegativeButton("취소",listener)
+            builder.show()
+        }
+
 
     }
 }
