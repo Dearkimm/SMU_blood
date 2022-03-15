1654cd774a3660c857b10411ca679f5d05d0fdb2
koharin <dudu@sookmyung.ac.kr>
2022-03-15 19:12:43 +0900

    Use userId in Comment, CommentData

--- app/src/main/java/org/smu/blood/api/database/Comment.kt
+++ app/src/main/java/org/smu/blood/api/database/Comment.kt
@@ -3,6 +3,7 @@ package org.smu.blood.api.database
 class Comment {
     var commentId: Int? = null
     var reviewId: Int? = null
+    var userId: String? = null
     var nickname: String? = null
     var time: String? = null
     var comment: String? = null
@@ -12,18 +13,20 @@ class Comment {
     constructor(
         commentId: Int?,
         reviewId: Int?,
+        userId: String?,
         nickname: String?,
         time: String?,
         comment: String?
     ) {
         this.commentId = commentId
         this.reviewId = reviewId
+        this.userId = userId
         this.nickname = nickname
         this.time = time
         this.comment = comment
     }
 
     override fun toString(): String {
-        return "Comment(commentId=$commentId, reviewId=$reviewId, nickname=$nickname, time=$time, comment=$comment)"
+        return "Comment(commentId=$commentId, reviewId=$reviewId, userId:$userId,nickname=$nickname, time=$time, comment=$comment)"
     }
 }
--- app/src/main/java/org/smu/blood/ui/board/CommentData.kt
+++ app/src/main/java/org/smu/blood/ui/board/CommentData.kt
@@ -3,6 +3,7 @@ package org.smu.blood.ui.board
 data class CommentData( //댓글 데이터
     val commentId : Int,
     val reviewId : Int,
+    val userId: String,
     val nickname : String,
     val time : String, //댓글 쓴 날짜
     val comment : String
